e1aa333af7c5110b6dc0966ad1fb01aeaedad3f1
natkhosh <t9219788607@gmail.com>
2022-01-27 23:26:56 +0300

    upd: docker-compose.yml

--- EPAM_weather_app/urls.py
+++ EPAM_weather_app/urls.py
@@ -19,10 +19,9 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('weather/', include('weather.urls')),
+    path('', include('weather.urls')),
     path('admin/', admin.site.urls),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler400 = "weather.views.handler400"
 handler404 = "weather.views.handler404"
